d309ca218f2bb129f79c0601e5d9d0b7036dec50
Michel Soliva <michel.soliva@cspconsultoria.com.br>
2019-10-11 10:38:29 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/soliva/entities/User.java
+++ src/main/java/br/com/soliva/entities/User.java
@@ -2,12 +2,20 @@ package br.com.soliva.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
